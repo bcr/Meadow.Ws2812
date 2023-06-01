2685b3564f92380c3a7257bf326571d32bcfa42d
Blake Ramsdell <blaker@gmail.com>
2023-06-01 08:51:36 -0700

    Clean out template file so it builds

--- MeadowApp.cs
+++ MeadowApp.cs
@@ -1,24 +1,18 @@
 ﻿using Meadow;
 using Meadow.Devices;
-using Meadow.Foundation;
-using Meadow.Foundation.Leds;
-using Meadow.Peripherals.Leds;
-using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace MeadowApp
 {
-	// Change F7FeatherV2 to F7FeatherV1 for V1.x boards
+    // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
     public class MeadowApp : App<F7FeatherV2>
 	{
-		RgbPwmLed onboardLed;
-
 		public override Task Run()
 		{
 			Resolver.Log.Info("Run...");
 
-			CycleColors(TimeSpan.FromMilliseconds(1000));
+			// Add code here
+
 			return base.Run();
 		}
 
@@ -26,41 +20,9 @@ namespace MeadowApp
 		{
 			Resolver.Log.Info("Initialize...");
 
-			onboardLed = new RgbPwmLed(
-				redPwmPin: Device.Pins.OnboardLedRed,
-				greenPwmPin: Device.Pins.OnboardLedGreen,
-				bluePwmPin: Device.Pins.OnboardLedBlue,
-				CommonType.CommonAnode);
+			// Add code here
 
 			return base.Initialize();
-		}
-
-		void CycleColors(TimeSpan duration)
-		{
-			Resolver.Log.Info("Cycle colors...");
-
-			while (true)
-			{
-				ShowColorPulse(Color.Blue, duration);
-				ShowColorPulse(Color.Cyan, duration);
-				ShowColorPulse(Color.Green, duration);
-				ShowColorPulse(Color.GreenYellow, duration);
-				ShowColorPulse(Color.Yellow, duration);
-				ShowColorPulse(Color.Orange, duration);
-				ShowColorPulse(Color.OrangeRed, duration);
-				ShowColorPulse(Color.Red, duration);
-				ShowColorPulse(Color.MediumVioletRed, duration);
-				ShowColorPulse(Color.Purple, duration);
-				ShowColorPulse(Color.Magenta, duration);
-				ShowColorPulse(Color.Pink, duration);
-			}
-		}
-
-		void ShowColorPulse(Color color, TimeSpan duration)
-		{
-			onboardLed.StartPulse(color, duration / 2);
-			Thread.Sleep(duration);
-			onboardLed.Stop();
 		}
 	}
 }
