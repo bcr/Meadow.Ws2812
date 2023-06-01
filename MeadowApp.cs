717263e9bb0a828bfeecef30dbe6d34869c41f73
Blake Ramsdell <blaker@gmail.com>
2023-06-01 10:52:55 -0700

    Add output of various test patterns

--- MeadowApp.cs
+++ MeadowApp.cs
@@ -1,5 +1,7 @@
 ﻿using Meadow;
 using Meadow.Devices;
+using Meadow.Hardware;
+using Meadow.Units;
 using System.Threading.Tasks;
 
 namespace MeadowApp
@@ -7,11 +9,16 @@ namespace MeadowApp
     // Change F7FeatherV2 to F7FeatherV1 for V1.x boards
     public class MeadowApp : App<F7FeatherV2>
 	{
+		private ISpiBus spiDevice;
+		private IDigitalOutputPort chipSelect;
+
 		public override Task Run()
 		{
 			Resolver.Log.Info("Run...");
 
-			// Add code here
+			spiDevice.Write(chipSelect, new byte[] { 0x88, 0x8C, 0xC8, 0xCC });
+			spiDevice.Write(chipSelect, new byte[] { 0b0101_0101, 0b1010_1010, 0b0101_0101, 0b1010_1010 });
+			spiDevice.Write(chipSelect, new byte[] { 0b0100_0000, 0b0110_0000, 0b0111_0000, 0b0111_1000 });
 
 			return base.Run();
 		}
@@ -20,7 +27,7 @@ namespace MeadowApp
 		{
 			Resolver.Log.Info("Initialize...");
 
-			// Add code here
+			spiDevice = Device.CreateSpiBus(new Frequency(3.2, Frequency.UnitType.Megahertz));
 
 			return base.Initialize();
 		}
